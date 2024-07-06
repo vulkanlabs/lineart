570383c250b7473fc4d0b6b3998a3dad2044819e
Antonio Pedro Vieira <aps.vieira95@gmail.com>
2024-07-06 20:22:29 -0300

    add dagster base
    Signed-off-by: Antonio Pedro Vieira <aps.vieira95@gmail.com>

---
+++ vulkan_dagster/assets.py
@@ -0,0 +1,45 @@
+
+from dagster import define_asset_job, Definitions
+from vulkan_dagster.nodes import HTTPConnection, HTTPConnectionConfig, NodeType, Context, NodeConfig, Transform
+
+context = Context(
+    data={
+        "cpf": "12345678900"
+    },
+    variables={}
+)
+
+config = HTTPConnectionConfig(
+    name="scr",
+    description="Get SCR score",
+    type=NodeType.CONNECTION,
+    url="http://127.0.0.1:5000",
+    method="GET",
+    headers={},
+    params={},
+) 
+n1 = HTTPConnection(config)
+
+config = NodeConfig(
+    name="transform",
+    description="Transform data",
+    type=NodeType.TRANSFORM,
+)
+
+# The function has to specify a set of parameters that is defined in other assets.
+def f2(scr):
+    return scr
+
+p2 = {"scr": "x"}
+n2 = Transform(config, f2, p2)
+
+vulkan_nodes = [n1, n2]
+dagster_nodes = [n.node() for n in vulkan_nodes]
+node_ids = [n.config.name for n in vulkan_nodes]
+
+job = define_asset_job("scr", selection=node_ids)
+
+defs = Definitions(
+    assets=dagster_nodes,
+    jobs=[job],
+)
---
+++ vulkan_dagster/nodes.py
@@ -0,0 +1,87 @@
+from dagster import asset, AssetIn
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+import requests
+
+
+class NodeType(Enum):
+    TRANSFORM = 1
+    CONNECTION = 2
+    BRANCH = 3
+
+
+class Status(Enum):
+    APPROVED = 1
+    DENIED = 2
+
+
+@dataclass
+class Context:
+    data: dict[str, Any]
+    variables: dict[str, Any]
+
+
+@dataclass
+class NodeConfig:
+    name: str
+    description: str
+    type: NodeType
+
+
+@dataclass
+class HTTPConnectionConfig(NodeConfig):
+    url: str
+    method: str
+    headers: dict
+    params: dict
+
+
+class Node(ABC):
+    
+    def __init__(self, config: NodeConfig):
+        self.config = config
+
+    @abstractmethod
+    def node(self):
+        pass
+
+
+class HTTPConnection(Node):
+
+    def __init__(self, config: HTTPConnectionConfig):
+        super().__init__(config)
+
+    def node(self):
+        return asset(
+            self._run,
+            name=self.config.name,
+        )
+
+    def _run(self):
+        response = requests.request(
+            method=self.config.method,
+            url=self.config.url,
+            headers=self.config.headers,
+            params=self.config.params,
+        )
+        if response.status_code == 200:
+            return response.json()
+        # handle exception in controller
+        raise Exception("Connection failed")
+    
+
+class Transform(Node):
+
+    def __init__(self, config: NodeConfig, func: Any, params: dict[str, Any]):
+        super().__init__(config)
+        self.func = func
+        self.params = params
+
+    def node(self):
+        return asset(
+            self.func,
+            name=self.config.name,
+            ins={key: AssetIn(key) for key, _ in self.params.items()},
+        )
