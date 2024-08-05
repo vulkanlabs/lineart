21a6e481eb0ff9949db23e76f9a3fc09eaa78ba6
Antonio Pedro Vieira <aps.vieira95@gmail.com>
2024-08-05 10:28:07 -0300

    wip: add draft frontend impl
    Signed-off-by: Antonio Pedro Vieira <aps.vieira95@gmail.com>

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import PersistentDrawerLeft from "./components";
+
+export default function Home() {
+  return (
+    <div>
+      <PersistentDrawerLeft />
+    </div>
+  );
+}
