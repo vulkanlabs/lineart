f0766045250b70de9e89caefac83d18b461791bb
Antonio Pedro Vieira <aps.vieira95@gmail.com>
2024-08-30 15:35:25 -0300

    refactor(vulkan): move package handling to environment module
    Signed-off-by: Antonio Pedro Vieira <aps.vieira95@gmail.com>

--- vulkan/vulkan/dagster/workspace.py
+++ vulkan/vulkan/dagster/workspace.py
@@ -1,13 +1,11 @@
 import importlib.util
 import os
 import sys
-import tarfile
-from shutil import unpack_archive
 
 from dagster import Definitions, EnvVar, IOManagerDefinition
 
 from vulkan.core.component import ComponentDefinition
-from vulkan.core.nodes import NodeFactory, InputNode
+from vulkan.core.nodes import InputNode, NodeFactory
 from vulkan.dagster.component import DagsterComponent
 from vulkan.dagster.io_manager import (
     DB_CONFIG_KEY,
@@ -88,11 +86,6 @@ def make_workspace_definition(policy: DagsterPolicy) -> Definitions:
     return definition
 
 
-_ARCHIVE_FORMAT = "gztar"
-_TAR_FLAGS = "w:gz"
-_EXCLUDE_PATHS = [".git", ".venv", ".vscode"]
-
-
 def configure_component_nodes(nodes, config):
     _nodes = []
     for node in nodes:
@@ -123,41 +116,6 @@ def extract_component_definition(file_location):
     raise ValueError("No component definition found in module")
 
 
-def pack_workspace(name: str, repository_path: str):
-    basename = f".tmp.{name}"
-    # TODO: In the future we may want to have an ignore list
-    filename = f"{basename}.{_ARCHIVE_FORMAT}"
-    with tarfile.open(name=filename, mode=_TAR_FLAGS) as tf:
-        for root, dirs, files in os.walk(repository_path):
-            # TODO: match regex instead of exact path
-            for path in _EXCLUDE_PATHS:
-                if path in dirs:
-                    dirs.remove(path)
-
-            for file in files:
-                file_path = os.path.join(root, file)
-                tf.add(
-                    file_path,
-                    arcname=os.path.relpath(file_path, repository_path),
-                )
-    with open(filename, "rb") as f:
-        repository = f.read()
-    os.remove(filename)
-    return repository
-
-
-def unpack_workspace(base_dir: str, name: str, repository: bytes):
-    workspace_path = os.path.join(base_dir, name)
-    filepath = f".tmp.{name}"
-    with open(filepath, "wb") as f:
-        f.write(repository)
-    unpack_archive(filepath, workspace_path, format=_ARCHIVE_FORMAT)
-
-    os.remove(filepath)
-
-    return workspace_path
-
-
 def add_workspace_config(
     base_dir: str,
     name: str,
@@ -174,27 +132,3 @@ def add_workspace_config(
                 f"      location_name: {name}\n"
             )
         )
-
-
-def find_package_entrypoint(file_location):
-    if not os.path.isdir(file_location):
-        if file_location.endswith(".py"):
-            return file_location
-        msg = (
-            "Entrypoint needs to be a python package or a python file, got: {}"
-        ).format(file_location)
-        raise ValueError(msg)
-
-    init_file = _find_first_init_file(file_location)
-    if init_file is None:
-        raise ValueError(
-            f"Could not find __init__.py file in directory: {file_location}"
-        )
-    return init_file
-
-
-def _find_first_init_file(file_location):
-    for root, _, files in os.walk(file_location):
-        for file in files:
-            if file == "__init__.py":
-                return os.path.join(root, file)
---
+++ vulkan/vulkan/environment/packing.py
@@ -0,0 +1,71 @@
+import os
+import tarfile
+from shutil import unpack_archive
+
+_ARCHIVE_FORMAT = "gztar"
+_TAR_FLAGS = "w:gz"
+_EXCLUDE_PATHS = [".git", ".venv", ".vscode"]
+
+
+def pack_workspace(name: str, repository_path: str):
+    basename = f".tmp.{name}"
+    # TODO: In the future we may want to have an ignore list
+    filename = f"{basename}.{_ARCHIVE_FORMAT}"
+    with tarfile.open(name=filename, mode=_TAR_FLAGS) as tf:
+        for root, dirs, files in os.walk(repository_path):
+            # TODO: match regex instead of exact path
+            for path in _EXCLUDE_PATHS:
+                if path in dirs:
+                    dirs.remove(path)
+
+            for file in files:
+                file_path = os.path.join(root, file)
+                tf.add(
+                    file_path,
+                    arcname=os.path.relpath(file_path, repository_path),
+                )
+    with open(filename, "rb") as f:
+        repository = f.read()
+    os.remove(filename)
+    return repository
+
+
+def unpack_workspace(base_dir: str, name: str, repository: bytes):
+    workspace_path = os.path.join(base_dir, name)
+    filepath = f".tmp.{name}"
+    with open(filepath, "wb") as f:
+        f.write(repository)
+    unpack_archive(filepath, workspace_path, format=_ARCHIVE_FORMAT)
+
+    os.remove(filepath)
+
+    return workspace_path
+
+
+def find_package_entrypoint(file_location: str):
+    """Find the first python init file in a directory.
+
+    NOTE: this function assumes that there is a single entrypoint in a
+    package. If there are multiple, it may fail to get definitions.
+    """
+    if not os.path.isdir(file_location):
+        if file_location.endswith(".py"):
+            return file_location
+        msg = (
+            "Entrypoint needs to be a python package or a python file, got: {}"
+        ).format(file_location)
+        raise ValueError(msg)
+
+    init_file = _find_first_init_file(file_location)
+    if init_file is None:
+        raise ValueError(
+            f"Could not find __init__.py file in directory: {file_location}"
+        )
+    return init_file
+
+
+def _find_first_init_file(file_location):
+    for root, _, files in os.walk(file_location):
+        for file in files:
+            if file == "__init__.py":
+                return os.path.join(root, file)
