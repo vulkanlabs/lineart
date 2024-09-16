49e7041006c648e9ba9672a3683636ca5165ddc3
Bruno Sztutman Kocinas <brunokocinas@gmail.com>
2024-09-16 19:18:13 -0300

    wip: user auth on front and back-end
    Signed-off-by: Bruno Sztutman Kocinas <brunokocinas@gmail.com>

--- vulkan-server/vulkan_server/app.py
+++ vulkan-server/vulkan_server/app.py
@@ -1,9 +1,11 @@
 from typing import Annotated
+import os
 
-from fastapi import Body, Depends, FastAPI, HTTPException
+from fastapi import Body, Depends, Header, FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from vulkan.core.run import RunStatus
+import requests
 
 from vulkan_server import routers, schemas
 from vulkan_server.db import DBSession, Run, StepMetadata, get_db
@@ -33,6 +35,50 @@ app.include_router(routers.policy_versions.router)
 
 
 logger = init_logger("vulkan_server")
+
+
+STACK_PROJECT_ID = os.getenv("STACK_PROJECT_ID")
+STACK_SECRET_SERVER_KEY = os.getenv("STACK_SECRET_SERVER_KEY")
+
+
+@app.middleware("http")
+async def auth_user(request: Request, call_next):
+    x_stack_access_token = request.headers.get("x-stack-access-token")
+    x_stack_refresh_token = request.headers.get("x-stack-refresh-token")
+    if not x_stack_access_token or not x_stack_refresh_token:
+        return await call_next(request)
+
+    url = "https://api.stack-auth.com/api/v1/users/me"
+    headers = {
+        "x-stack-access-type": "server",
+        "x-stack-project-id": STACK_PROJECT_ID,
+        "x-stack-secret-server-key": STACK_SECRET_SERVER_KEY,
+        "x-stack-access-token": x_stack_access_token,
+        "x-stack-refresh-token": x_stack_refresh_token,
+    }
+    response = requests.get(url, headers=headers)
+    if response.json()['id'] is None:
+        return HTTPException(status_code=401, detail="Unauthorized")
+    return await call_next(request)
+
+
+@app.get("/users/me")
+def read_users_me(
+    x_stack_access_token: Annotated[str, Header()],
+    x_stack_refresh_token: Annotated[str, Header()],
+):
+    url = "https://api.stack-auth.com/api/v1/users/me"
+    headers = {
+        "x-stack-access-type": "server",
+        "x-stack-project-id": STACK_PROJECT_ID,
+        "x-stack-secret-server-key": STACK_SECRET_SERVER_KEY,
+        "x-stack-access-token": x_stack_access_token,
+        "x-stack-refresh-token": x_stack_refresh_token,
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    logger.info(data)
+    return {"user": data}
 
 
 @app.get("/runs/{run_id}", response_model=schemas.Run)
