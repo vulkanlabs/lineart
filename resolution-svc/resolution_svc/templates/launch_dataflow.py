cb01aa446d8d740b8727a4a94c07b391e5a3dfd6
Antonio Pedro Vieira <apsvieira95@gmail.com>
2024-11-16 22:08:00 -0300

    refactor(resolution): working version of flex-template launch script
    Signed-off-by: Antonio Pedro Vieira <apsvieira95@gmail.com>

--- resolution-svc/resolution_svc/templates/launch_dataflow.py
+++ resolution-svc/resolution_svc/templates/launch_dataflow.py
@@ -4,56 +4,33 @@ from argparse import ArgumentParser
 
 from apache_beam.options.pipeline_options import PipelineOptions
 from vulkan.beam.pipeline import BeamPipelineBuilder, DataEntryConfig
-from vulkan.core.policy import Policy
 from vulkan.environment.loaders import resolve_policy
 
 logger = logging.getLogger("uvicorn.error")
 logger.setLevel(logging.INFO)
 
-GCP_PROJECT_ID="vulkan-dev-a8b0"
-GCP_REGION="us-central1"
-GCP_DATAFLOW_WORKER_SA="beam-worker-service-account"
-GCP_DATAFLOW_TEMP_LOCATION="vulkan-dev-beam-temp"
-GCP_DATAFLOW_STAGING_LOCATION="vulkan-dev-beam-temp/staging"
-GCP_DATAFLOW_OUTPUT_BUCKET="vulkan-dev-beam-results"
-
-# GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
-# GCP_REGION = os.getenv("GCP_REGION")
-# GCP_DATAFLOW_WORKER_SA = os.getenv("GCP_DATAFLOW_WORKER_SA")
-# GCP_DATAFLOW_TEMP_LOCATION = os.getenv("GCP_DATAFLOW_TEMP_LOCATION")
-# GCP_DATAFLOW_STAGING_LOCATION = os.getenv("GCP_DATAFLOW_STAGING_LOCATION")
-# GCP_DATAFLOW_OUTPUT_BUCKET = os.getenv("GCP_DATAFLOW_OUTPUT_BUCKET")
-
-
 def launch_pipeline(
     image: str,
     data_sources: dict[str, str],
-    policy: Policy,
+    module_name: str,
+    components_path: str,
     output_path: str,
     config_variables: dict[str, str] | None = None,
+    other_args: list[str] = [],
 ):
+    policy = resolve_policy(module_name, components_path)
+
     data_sources = {
         name: DataEntryConfig(source=source) for name, source in data_sources.items()
     }
-    # worker_options = ["--machine_type=n1-standard-4"]
-    # sdk = [
-        # "--experiments=use_runner_v2",
-        # f"--sdk_container_image={image}",
-        # "--sdk_location=container",
-    # ]
-    # flags = sdk + worker_options
 
-    pipeline_options = PipelineOptions()
-    # pipeline_options.view_as(StandardOptions).runner = "DataflowRunner"
-    # google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
-    # google_cloud_options.project = GCP_PROJECT_ID
-    # google_cloud_options.region = GCP_REGION
-    # google_cloud_options.temp_location = f"gs://{GCP_DATAFLOW_TEMP_LOCATION}"
-    # google_cloud_options.staging_location = f"gs://{GCP_DATAFLOW_STAGING_LOCATION}"
-    # google_cloud_options.service_account_email = (
-    #     f"{GCP_DATAFLOW_WORKER_SA}@{GCP_PROJECT_ID}.iam.gserviceaccount.com"
-    # )
+    pipeline_args = [
+        "--save_main_session",
+        "--sdk_container_image", image,
+        "--sdk_location", "container",
+    ] + other_args
 
+    pipeline_options = PipelineOptions(pipeline_args)
     pipeline = BeamPipelineBuilder(
         policy=policy,
         output_path=output_path,
@@ -67,8 +44,8 @@ def launch_pipeline(
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    # Backtest config args
     parser.add_argument("--image", type=str)
+    # Backtest config args
     parser.add_argument("--output_path", type=str)
     parser.add_argument("--data_sources", type=str)
     parser.add_argument("--config_variables", type=str)
@@ -84,11 +61,12 @@ if __name__ == "__main__":
     else:
         config_variables = None
 
-    policy = resolve_policy(args.module_name, args.components_path)
     launch_pipeline(
+        image = args.image,
         data_sources=data_sources,
-        config_variables=config_variables,
-        policy=policy,
-        image=args.image,
+        module_name=args.module_name,
+        components_path=args.components_path,
         output_path=args.output_path,
+        config_variables=config_variables,
+        other_args=unknown_args,
     )
--- scripts/launch-flex-template.sh
+++ scripts/launch-flex-template.sh
@@ -4,6 +4,7 @@ POLICY_ID=$1
 REGION="us-central1"
 LAUNCHER_SERVICE_ACCOUNT="beam-launcher-service-account@vulkan-dev-a8b0.iam.gserviceaccount.com"
 WORKER_SERVICE_ACCOUNT="beam-worker-service-account@vulkan-dev-a8b0.iam.gserviceaccount.com"
+MACHINE_TYPE="n1-standard-2"
 GCP_DATAFLOW_TEMP_LOCATION="gs://vulkan-dev-beam-temp"
 GCP_DATAFLOW_STAGING_LOCATION="gs://vulkan-dev-beam-temp/staging"
 
@@ -13,6 +14,7 @@ gcloud dataflow flex-template run "test-run-`date +%Y%m%d-%H%M%S`" \
   --service-account-email ${WORKER_SERVICE_ACCOUNT} \
   --temp-location ${GCP_DATAFLOW_TEMP_LOCATION} \
   --staging-location ${GCP_DATAFLOW_STAGING_LOCATION} \
+  --worker-machine-type ${MACHINE_TYPE} \
   --parameters image="us-central1-docker.pkg.dev/vulkan-dev-a8b0/docker-images/${POLICY_ID}:beam" \
   --parameters output_path="gs://vulkan-dev-beam-results/test/policy_id" \
   --parameters data_sources='{"input_node": "gs://vulkan-dev-upload-service-bucket/data/e385da7c-b5de-4bd5-85e5-f30897a79aee/1d909f6a-37a4-4cc2-bf48-09e3ce4d1b5d"}' \
