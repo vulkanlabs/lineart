bde28c2a77acbc6809a23f487424454acd2ce32d
Bruno Sztutman Kocinas <brunokocinas@gmail.com>
2025-04-04 17:58:44 -0300

    refactor(vulkan-dagster): create dagster workspace from policy spec
    Signed-off-by: Bruno Sztutman Kocinas <brunokocinas@gmail.com>

---
+++ vulkan-dagster/scripts/venv_create.sh
@@ -0,0 +1,9 @@
+set -ex
+workspace_path=$1
+
+# Install dependencies and the policy itself
+uv init --bare ${workspace_path}
+cd ${workspace_path}
+uv add ${VULKAN_SERVER_PATH}/vulkan-public
+uv add ${VULKAN_SERVER_PATH}/vulkan
+uv sync
