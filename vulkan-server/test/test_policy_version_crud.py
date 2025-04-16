b8821810316c0c53d5135181c7db2f5e1b96f3e6
Antonio Pedro Vieira <apsvieira95@gmail.com>
2025-04-16 17:40:03 -0300

    refactor: remove resolution svc and usages
    Signed-off-by: Antonio Pedro Vieira <apsvieira95@gmail.com>

--- vulkan-server/test/test_policy_version_crud.py
+++ vulkan-server/test/test_policy_version_crud.py
@@ -12,6 +12,8 @@ pytestmark = pytest.mark.integration
 
 
 def test_policy_version_crud_create(context):
+    input_schema = {"cpf": "str"}
+    empty_spec = {"nodes": [], "input_schema": input_schema}
     # Create a policy
     policy_id = vulkan.policy.create_policy(
         ctx=context,
@@ -20,14 +22,13 @@ def test_policy_version_crud_create(context):
     )
     assert policy_id is not None
     assert len(policy_id) > 0
-
     policy_version = vulkan.policy_version.create(
         ctx=context,
         policy_id=policy_id,
         version_name="v1.0",
-        input_schema={"cpf": "str"},
+        input_schema=input_schema,
         requirements=[],
-        spec={},
+        spec=empty_spec,
     )
     assert policy_version is not None
     assert policy_version["requirements"] == []
@@ -42,8 +43,21 @@ def test_policy_version_crud_create(context):
     assert policy_version_from_read["policy_version_id"] == policy_version_id
     assert policy_version == policy_version_from_read
 
+    vulkan.policy_version.delete_policy_version(
+        ctx=context,
+        policy_version_id=policy_version_id,
+    )
+
 
 def test_policy_version_crud_update(context):
+    input_schema = {"cpf": "str"}
+    empty_spec = {
+        "nodes": [],
+        "input_schema": input_schema,
+        "config_variables": None,
+        "output_callback": None,
+    }
+
     # Create a policy
     policy_id = vulkan.policy.create_policy(
         ctx=context,
@@ -57,28 +71,33 @@ def test_policy_version_crud_update(context):
         ctx=context,
         policy_id=policy_id,
         version_name="v1.0",
-        input_schema={"cpf": "str"},
+        input_schema=input_schema,
         requirements=[],
-        spec={},
+        spec=empty_spec,
     )
     assert policy_version is not None
     assert policy_version["requirements"] == []
-    assert policy_version["spec"] == {}
+    assert policy_version["spec"] == empty_spec
 
     policy_version_updated = vulkan.policy_version.update(
         ctx=context,
         policy_version_id=policy_version["policy_version_id"],
         version_name="v1.1",
-        input_schema={"cpf": "int"},
+        input_schema=input_schema,
         requirements=["numpy"],
-        spec={},
+        spec=empty_spec,
     )
     assert policy_version_updated is not None
     assert policy_version_updated["requirements"] == ["numpy"]
-    assert policy_version_updated["spec"] == {}
+    assert policy_version_updated["spec"] == empty_spec
     assert (
         policy_version_updated["policy_version_id"]
         == policy_version["policy_version_id"]
     )
     assert policy_version_updated["alias"] == "v1.1"
-    assert policy_version_updated["input_schema"] == {"cpf": "int"}
+    assert policy_version_updated["input_schema"] == input_schema
+
+    vulkan.policy_version.delete_policy_version(
+        ctx=context,
+        policy_version_id=policy_version["policy_version_id"],
+    )
