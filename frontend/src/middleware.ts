d8797d62002152d71a8a011afa2408b86703880c
Antonio Pedro Vieira <apsvieira95@gmail.com>
2025-04-25 11:53:51 -0300

    chore(frontend): remove stack-auth dependency
    Signed-off-by: Antonio Pedro Vieira <apsvieira95@gmail.com>

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,6 @@
-import { stackServerApp } from "@/stack";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-    const user = await stackServerApp.getUser();
-    if (!user) {
-        return NextResponse.redirect(new URL("/handler/sign-in", request.url));
-    }
     return NextResponse.next();
 }
 
